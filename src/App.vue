196efdefc3e587432de8e5c1681c6e483e05b60f
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2023-01-20 16:34:51 +0100

    esercizio completato

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import AppTitle from "./components/AppTitle.vue"
+export default {
+  name: "App",
+  components: { AppTitle }
+}
+</script>
+
+<template>
+  <app-title></app-title>
+</template>
+
+<style>
+
+</style>
